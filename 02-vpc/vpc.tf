e265336f52c596f8a8c88f34fca99e92a276b3b2
pbahrs <pbahrs@us.ibm.com>
2020-05-05 09:42:38 -0400

    Update vpc.tf

--- 02-vpc/vpc.tf
+++ 02-vpc/vpc.tf
@@ -76,7 +76,7 @@ resource ibm_is_public_gateway gateway {
 resource ibm_is_subnet subnet {
   count           = "3"
 
-  name            = "${var.unique_id}-subnet-${count.index + 1}"
+  name            = "${var.unique_id}-subnet${count.index + 1}"
   vpc             = "${ibm_is_vpc.vpc.id}"
   zone            = "${var.ibm_region}-${count.index + 1}"
   ipv4_cidr_block = "${element(ibm_is_vpc_address_prefix.subnet_prefix.*.cidr, count.index)}"
