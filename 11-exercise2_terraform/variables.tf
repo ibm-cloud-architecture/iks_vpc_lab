35b95742d008fe36887e98bfc0c33d2c0c110322
Cheok Swin Voon <cheoksv@my.ibm.com>
2020-05-19 21:23:26 +0800

    replace with key protect

--- 11-exercise2_terraform/variables.tf
+++ 11-exercise2_terraform/variables.tf
@@ -1,41 +1,31 @@
+##############################################################################
+# Account Variables
+##############################################################################
 
-
-variable "plan" {
-  default = "standard"
-  description = "Cloud Database Plan"
-}
-variable "ibm_region" {
+variable ibm_region {
   default = "us-south"
-  description = "Cloud Database region "
+  description = "IBM Cloud region where all resources will be deployed"
 }
-variable "db_name" {
-  default = "ex2_test_db_lab"
-  description = "Database name"
+
+variable unique_id {
+  default = "kms"
+  description = "A unique id to use in naming conventions for resources"
 }
+
 variable "resource_group_name" {
   description = "IBM Cloud Resource Group Name"
 }
-variable "db_admin_password" {
-  default = "password123"
-  description = "Database administration password"
+
+##############################################################################
+# Key Protect Variables
+##############################################################################
+
+variable kms_plan {
+  description = "the plan to use for provisioning key protect instance"
+  default     = "tiered-pricing"
 }
 
-variable "db_memory_allocation" {
-  default = "3072"
-  description = "The amount of memory in megabytes for the database, split across all members. If not specified, the default setting of the database service is used, which can vary by database type."
-}
-
-variable "db_disk_allocation" {
-  default = "61440"
-  description = "The amount of disk space for the database, split across all members. If not specified, the default setting of the database service is used, which can vary by database type."
-}
-
-variable "user_username"{
-  default = "admin"
-  description = "name of the admin user for postgres database"
-}
-
-variable "user_password" {
-  default = "password123"
-  description = "password for the lab database"
+variable kms_root_key_name {
+  description = "the root key name for the key protect instance"
+  default     = "root_key"
 }
