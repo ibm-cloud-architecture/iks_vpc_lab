903b091f21927e2af99c8f9174deed363aff301c
Steve Cotugno <steve.cotugno@us.ibm.com>
2020-07-08 18:00:02 -0700

    fixed map element reference

--- 03-ocp_cluster/cluster.tf
+++ 03-ocp_cluster/cluster.tf
@@ -42,16 +42,17 @@ resource "ibm_container_vpc_cluster" "cluster" {
   cos_instance_crn  = ibm_resource_instance.rh_cos_instance.id
   entitlement       = "cloud_pak"
 
+.*.id
   zones {
-    subnet_id = element(data.ibm_schematics_output.vpc_workspace.output_values.subnet_ids, 0)
+    subnet_id = element(data.ibm_schematics_output.vpc_workspace.output_values.subnet_ids.*.id, 0)
     name      = "${var.ibm_region}-1"
   }
   zones {
-    subnet_id = element(data.ibm_schematics_output.vpc_workspace.output_values.subnet_ids, 1)
+    subnet_id = element(data.ibm_schematics_output.vpc_workspace.output_values.subnet_ids.*.id, 1)
     name      = "${var.ibm_region}-2"
   }
   zones {
-    subnet_id = element(data.ibm_schematics_output.vpc_workspace.output_values.subnet_ids, 2)
+    subnet_id = element(data.ibm_schematics_output.vpc_workspace.output_values.subnet_ids.*.id, 2)
     name      = "${var.ibm_region}-3"
   }
 }
