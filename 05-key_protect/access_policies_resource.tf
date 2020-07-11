820be9723df606021b24e91ab1909c8ed52f5ab9
Steve Cotugno <steve.cotugno@us.ibm.com>
2020-07-11 08:31:27 -0700

    converted to tf v0.12

--- 05-key_protect/access_policies_resource.tf
+++ 05-key_protect/access_policies_resource.tf
@@ -12,7 +12,7 @@
 
 
 resource ibm_iam_access_group_policy labadmin_policy {
-  access_group_id = "${data.ibm_schematics_output.groups_output.output_values.labadmin_group_id}"
+  access_group_id = data.ibm_schematics_output.groups_output.output_values.labadmin_group_id
   roles           = ["${var.labadmin_access}"]
 
   resources = [{
@@ -22,7 +22,7 @@ resource ibm_iam_access_group_policy labadmin_policy {
 }
 
 resource ibm_iam_access_group_policy kms_engineer_access_policy {
-  access_group_id = "${data.ibm_schematics_output.groups_output.output_values.labuser_group_id}"
+  access_group_id = data.ibm_schematics_output.groups_output.output_values.labuser_group_id
   roles           = ["${var.labuser_access}"]
 
   resources = [{
